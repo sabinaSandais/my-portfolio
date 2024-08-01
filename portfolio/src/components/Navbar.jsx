b1068fd8057eeaa768c4226b06b26f4a50a86b67
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-08-01 16:40:08 +0200

    Update Navbar.jsx

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <nav >
+      <a href="/">Portfolio</a>
+      <div>
+        <ul>
+          <li >
+            <a href="#about">About</a>
+          </li>
+          <li >
+            <a href="experience">Experience</a>
+          </li>
+          <li >
+            <a href="#projects">Projects</a>
+          </li> <li >
+            <a href="#about">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   )
 }
 
