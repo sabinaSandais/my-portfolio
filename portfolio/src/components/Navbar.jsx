cf738cb8dc9db462999521dfa8b96a4c25ab3ef0
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-08-01 18:42:40 +0200

    updated

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './Navbar.module.css';
+//import {getImageUrl} from '../utils/getImageUrl';
 
 const Navbar = () => {
   return (
