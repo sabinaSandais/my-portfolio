4b1b229a9eb5724bf4ca82e0a160d34c56361888
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-08-03 14:21:53 +0200

    Update Hero.jsx

--- portfolio/src/components/Hero/Hero.jsx
+++ portfolio/src/components/Hero/Hero.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 
 const Hero = () => {
   return (
+   <section>
     <div>
-      
+        <h1>Hi, I'm Sabina Sandais</h1>
+        <p>I am a full-stack developer.
+            Reach out if you'd like to learn more!
+        </p>
+        <a href="mailto:sabinasandais@gmail.com"> Contact Me</a>
     </div>
+   </section>
   )
 }
 
