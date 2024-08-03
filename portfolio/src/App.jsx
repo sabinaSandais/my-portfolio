aa5d72989e0e9efba4ef0d1aca8ee558256df7f3
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-08-03 14:02:40 +0200

    Hero section

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,11 +1,14 @@
 import styles from './App.module.css';
+import Hero from './components/Hero/Hero';
 import Navbar from './components/Navbar';
 
 function App() {
   
 
   return (
-    <div className={styles.App}><Navbar />
+    <div className={styles.App}>
+      <Navbar />
+      <Hero />
     
     </div>
   )
