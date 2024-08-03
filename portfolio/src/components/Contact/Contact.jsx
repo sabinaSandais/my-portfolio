509f47c04a9a4f02ac517a11d6205a8a05fc4d5b
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-08-04 00:14:24 +0200

    Update Contact.jsx

--- portfolio/src/components/Contact/Contact.jsx
+++ portfolio/src/components/Contact/Contact.jsx
@@ -13,7 +13,7 @@ import { getImageUrl } from "../../utils";
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:myemail@email.com">myemail@email.com</a>
+          <a href="mailto:sabinasandais@gmail.com">myemail@email.com</a>
         </li>
         <li className={styles.link}>
           <img
