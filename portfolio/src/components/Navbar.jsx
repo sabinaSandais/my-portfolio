28df2c833d547cd1f55d649153f21e5a65912e64
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-08-03 13:43:55 +0200

    Update Navbar.jsx

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './Navbar.module.css';
 import {getImageUrl} from '../utils';
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] =useState(false);
   return (
     <nav className={styles.navbar}>
       <a className= {styles.title}
        href="/">Portfolio</a>
       <div className={styles.menu}>
-        <img className={styles.menuBtn} src={getImageUrl("nav/menuIcon.png")} alt="menu.btn" />
+        <img className={styles.menuBtn} src={ menuOpen ? getImageUrl("nav/closeIcon.png") : getImageUrl("nav/menuIcon.png")} alt="menu-button"   onClick={() => setMenuOpen(!menuOpen)} />
         <ul className={styles.menuItems}>
           <li >
             <a href="#about">About</a>
