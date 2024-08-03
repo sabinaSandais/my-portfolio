c9f44eed61604099bba51efbf36e792888ba0f4d
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-08-03 22:06:52 +0200

    Update Experience.jsx

--- portfolio/src/components/Experience/Experience.jsx
+++ portfolio/src/components/Experience/Experience.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React from 'react';
+import skills from "../../data/skills.json";
+import { getImageUrl } from '../../utils';
 
 const Experience = () => {
   return (
-    <div>
-      Experience
-    </div>
+    <section id='experience'>
+        <h2>Experience</h2>
+        <div>
+            <div>
+                {skills.map((skill, id) =>{
+                    return(
+                        <div key={id}>
+                            <div>
+                                <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+                            </div>
+                            <p>{skill.title}</p>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+      
+    </section>
   )
 }
 
