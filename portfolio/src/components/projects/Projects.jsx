68f12b4810aeb84e21492e6849c17686e6c01ae3
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-08-14 22:17:31 +0200

    Update Projects.jsx

--- portfolio/src/components/projects/Projects.jsx
+++ portfolio/src/components/projects/Projects.jsx
@@ -17,7 +17,7 @@ const Projects = () => {
                 className={styles.image}
               />
               <h3 className={styles.title}>{project.title}</h3>
-              <p>{project.description}</p>
+              <p className={styles.description}>{project.description}</p>
               <ul>
                 {project.skills.map((skill, id) => {
                   <li key={id}>{skill}</li>;
