f64bafbb3acf1abc8ef20a7ebf3dd0a8283c11c1
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-08-14 22:01:51 +0200

    Update Projects.jsx

--- portfolio/src/components/projects/Projects.jsx
+++ portfolio/src/components/projects/Projects.jsx
@@ -7,10 +7,18 @@ const Projects = () => {
       <h2>Projects</h2>
       <div>
         {projects.map((project, id) => {
-            return<div key={id}>
-                 
-
+          return (
+            <div key={id}>
+              <img src={project.imgSrc} alt={`Image of${project.title}`} />
+              <h3>{project.title}</h3>
+              <p>{project.description}</p>
+              <ul>
+                {project.skills.map((skill, id) => {
+                  <li key={id}>{skill}</li>;
+                })}
+              </ul>
             </div>
+          );
         })}
       </div>
     </section>
