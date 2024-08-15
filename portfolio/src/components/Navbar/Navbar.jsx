214bb9d8f68fdefc443eed4503ab6bb931e78628
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-08-15 13:27:07 +0200

    Update Navbar.jsx

--- portfolio/src/components/Navbar/Navbar.jsx
+++ portfolio/src/components/Navbar/Navbar.jsx
@@ -1,34 +1,45 @@
-import React, {useState} from 'react';
-import styles from './Navbar.module.css';
-import {getImageUrl} from '../../utils';
+import React, { useState } from "react";
+import styles from "./Navbar.module.css";
+import { getImageUrl } from "../../utils";
 
 const Navbar = () => {
-  const [menuOpen, setMenuOpen] =useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <nav className={styles.navbar}>
-      <a className= {styles.title}
-       href="/">Portfolio</a>
+      <a className={styles.title} href="/">
+        Portfolio
+      </a>
       <div className={styles.menu}>
-        <img className={styles.menuBtn} src={ menuOpen ? getImageUrl("nav/closeIcon.png") : getImageUrl("nav/menuIcon.png")} alt="menu-button"   onClick={() => setMenuOpen(!menuOpen)} />
+        <img
+          className={styles.menuBtn}
+          src={
+            menuOpen
+              ? getImageUrl("nav/closeIcon.png")
+              : getImageUrl("nav/menuIcon.png")
+          }
+          alt="menu-button"
+          onClick={() => setMenuOpen(!menuOpen)}
+        />
         <ul
           className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
           onClick={() => setMenuOpen(false)}
         >
-          <li >
+          <li>
             <a href="#about">About</a>
           </li>
-          <li >
-            <a href="experience">Experience</a>
+          <li>
+            <a href="#experience">Experience</a>
           </li>
-          <li >
+          <li>
             <a href="#projects">Projects</a>
-          </li> <li >
-            <a href="#about">Contact</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
           </li>
         </ul>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
