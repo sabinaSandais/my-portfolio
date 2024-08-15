b59d1a6d7f2c16aa21d94e6212718f4ffad07555
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-08-15 13:35:14 +0200

    Update App.jsx

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -4,7 +4,7 @@ import Contact from "./components/Contact/Contact";
 import Experience from "./components/Experience/Experience";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
-import Projects from "./components/projects/Projects";
+import Projects from "./components/Projects/Projects";
 
 function App() {
   return (
